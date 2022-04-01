5e1c2947cda996d6619ff6587f2084f375714232
unknown <G00411347@gmit.ie>
2022-04-01 23:09:18 +0100

    Starting testing code and data

--- analysis.py
+++ analysis.py
@@ -15,14 +15,6 @@ data.isnull().sum()
 
 up_dt = {}
 
-for r in data:
-    if r["class"] == "Iris-setosa":
-        row = {'sepallength': r['sepallength'],
-            'sepalwidth': r['sepalwidth'],
-            'petallength': r['petallength'],
-            'petalwidth': r['petalwidth'],
-            'class': '1'}
-    up_dt.append(row)
   
 print(up_dt)
 sb.scatterplot(x='petallength', y='petalwidth',
@@ -37,22 +29,7 @@ sb.scatterplot(x='sepalwidth', y='sepallength',
                 hue='class', data=data, )
 plt.legend()
 plt.savefig('testMultiSave2.png')
-  
 
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
-
-x = data['sepallength']
-y = data['sepalwidth']
-z = data['class']
-
-ax.set_xlabel("sepallength")
-ax.set_ylabel("sepalwidth")
-ax.set_zlabel("class")
-
-ax.scatter(x, y, z)
 
 plt.show()
 
