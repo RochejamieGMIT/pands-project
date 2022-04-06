2d7fc03b3e5aca51fa4c42f00b77e71c648954f7
unknown <G00411347@gmit.ie>
2022-04-06 20:23:29 +0100

    Testing different data analysis tools

--- analysis.py
+++ analysis.py
@@ -1,35 +1,39 @@
 import pandas as pd
+import pandas_profiling as pp
 
 import seaborn as sb
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # reading csv files
-data =  pd.read_csv('iris_csv.csv')
+ids =  pd.read_csv('iris_csv.csv')
 #print(df)
-data.head()
-data.shape
-data.info()
-print(data.describe())
-data.isnull().sum()
+ids.head()
+#print(ids.shape)
+report = pp.ProfileReport(ids)
 
-up_dt = {}
-
-  
-print(up_dt)
-sb.scatterplot(x='petallength', y='petalwidth',
-               hue='class', data=data, )
-
-
-plt.legend()
-plt.savefig('testMultiSave.png')
-
- 
-sb.scatterplot(x='sepalwidth', y='sepallength',
-                hue='class', data=data, )
-plt.legend()
-plt.savefig('testMultiSave2.png')
-
-
-plt.show()
-
+report.to_file('profile_report.html') # To preview data had to create a .html file as vs code could not open it
+#data.info()
+#print(data.describe())
+#data.isnull().sum()
+#
+#up_dt = {}
+#
+#  
+#print(up_dt)
+#sb.scatterplot(x='petallength', y='petalwidth',
+#               hue='class', data=data, )
+#
+#
+#plt.legend()
+#plt.savefig('testMultiSave.png')
+#
+# 
+#sb.scatterplot(x='sepalwidth', y='sepallength',
+#                hue='class', data=data, )
+#plt.legend()
+#plt.savefig('testMultiSave2.png')
+#
+#
+#plt.show()
+#
